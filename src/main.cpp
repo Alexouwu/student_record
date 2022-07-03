c80d91799adea2f315e22fd3f460b35672560132
Alexouwu <alejandro.arriaga.uwusito@gmail.com>
2022-07-03 14:52:12 -0500

    remove ns

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include "../lib/COURSE/COURSE.h"
 #include "../lib/GRADE/GRADE.h"
 #include "../lib/STUDENT/STUDENT.h"
-// #include "./lib/STUDENT/STUDENT.cpp"
-
-using namespace std;
 
 STUDENT my_student(1, "Hilda Jones");
 COURSE my_course(7, "Physics 101", 3);
 GRADE my_grade(1, 7, 'B');
 
 int main() {
-  cout << "Student: " << my_student.getName() << endl;
-  cout << "Course: " << my_course.getName() << endl;
-  cout << "Credits: " << my_course.getCredits() << endl;
-  cout << "Grade:wqwqw " << my_grade.getGrade() << endl;
+  std::cout << "Student: " << my_student.getName() << std::endl;
+  std::cout << "Course: " << my_course.getName() << std::endl;
+  std::cout << "Credits: " << my_course.getCredits() << std::endl;
+  std::cout << "Grade:wqwqw " << my_grade.getGrade() << std::endl;
   return (0);
 }
